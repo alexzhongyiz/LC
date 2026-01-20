b8c9a0de6b2b409f8ac71ef89195807b53359611
alexzhongyiz <48136557+alexzhongyiz@users.noreply.github.com>
2026-01-19 22:19:01 -0600

    Remove timer decorator and solve function
    Removed the timer decorator and the solve function.

--- testsettings.py
+++ testsettings.py
@@ -7,22 +7,7 @@ import functools
 import collections
 import time
 
-def timer(func):
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-        start_time = time.perf_counter()
-        result = func(*args, **kwargs)
-        end_time = time.perf_counter()
-        print(f'\n[Execution time: {end_time - start_time:.6f} seconds]')
-        return result
-    return wrapper
+
 
 # Write your function here
 
-
-@timer
-def solve():
-    # write your unit tests here
-    return
-if __name__ == '__main__':
-    solve()
